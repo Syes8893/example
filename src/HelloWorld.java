3632cfe8e7b49ffcf15382cf5a461b072e0d909d
Lucas Petre <L.D.Petre@student.tudelft.nl>
2023-01-10 14:29:35 +0100

    Example 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class HelloWorld {
 
 	public static void main (String[] args){
-		Scanner sc = new Scanner(System.in);
-		sc.nextLine();
+		Selection selection = new Selection("Lucas");
 	}
 
 }
---
+++ src/Selection.java
@@ -0,0 +1,11 @@
+public class Selection {
+
+	private String name;
+
+	//Constructor, allows for passing values when creating a new instance of this class
+	public Selection(String name){
+		//set local variable (this.name) to variable passed in the constructor (name)
+		this.name = name;
+	}
+
+}
